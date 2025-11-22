beaed7b5c9bfc8db1aafd5a97e82c9ab0e743b14
hongltgch230156-prog <hongltgch230156@fpt.edu.vn>
2025-11-22 19:11:26 +0700

    Add migration files for Azure SQL

--- Service_URL_Shorten/Migrations/20251122121045_InitAzureDb.Designer.cs
+++ Service_URL_Shorten/Migrations/20251122121045_InitAzureDb.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Service_URL_Shorten.Data;
 
@@ -11,9 +12,11 @@ using Service_URL_Shorten.Data;
 namespace Service_URL_Shorten.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251122121045_InitAzureDb")]
+    partial class InitAzureDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Service_URL_Shorten/Migrations/20251122121045_InitAzureDb.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Service_URL_Shorten.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitAzureDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ClickCount",
+                table: "Urls",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClickCount",
+                table: "Urls");
+        }
+    }
+}
